ad34f8d43d8f0e8741ebba355d15bc923da0dc2f
Wang Wenxiang <wenxiang.wang@gmail.com>
2016-04-07 01:34:43 -0400

    fixed shell command error

--- start.sh
+++ start.sh
@@ -5,11 +5,13 @@ GUNICORN=/usr/bin/gunicorn
 ROOT=$PWD
 PID=/var/run/gunicorn.pid
 APP=driver:app
+PLUGIN=/run/docker/plugins
 
 if [ -f $PID ]; then rm $PID; fi
+if [ ! -d "$PLUGIN" ]; then mkdir $PLUGIN; fi
 
 $GUNICORN -u root --chdir $ROOT --pid=$PID \
--b unix:///run/docker/plugins/daolinet.sock $APP \
+-b unix://$PLUGIN/daolinet.sock $APP \
 --timeout=5 \
 --log-level=debug \
 --workers 1 \
